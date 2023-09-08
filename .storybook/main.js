79ad36cd110114eafcf9fcaf0831500cfa278d55
Michael Adamek <michael.adamek@hmhco.com>
2023-09-08 14:21:04 -0400

    Fixed missing sass issue.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,27 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
+
+import { mergeConfig } from 'vite';
+
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  stories: [ 
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)", 
+    "../src/components/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  core: {
+    builder: '@storybook/builder-vite',
   },
   docs: {
     autodocs: "tag",
   },
 };
+
 export default config;
