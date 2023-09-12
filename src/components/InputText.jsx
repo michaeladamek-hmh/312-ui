6fe234684f1779e52f7d5f55cc753730e16bd473
Michael Adamek <michael.adamek@hmhco.com>
2023-09-12 15:06:24 -0400

    Updated props.

---
+++ src/components/InputText.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+InputText.propTypes = {
+  correct: PropTypes.bool,
+  incorrect: PropTypes.bool,
+  value: PropTypes.string,
+  disabled: PropTypes.bool,
+	grade: PropTypes.oneOf(['g35', 'g68', 'g912'])
+}
+
+export function InputText({ correct, incorrect, value, disabled, grade }) {
+	const isCorrect = correct ? 'correct' : '';
+	const isIncorrect = incorrect ? 'incorrect' : '';
+
+  return (
+    <div className={ grade }>
+      <input 
+        type="text" 
+        className={` ${isCorrect} ${isIncorrect}`}
+        value={ value }
+        disabled={ disabled }
+      />
+    </div>
+  )
+}
---
+++ src/components/InputText.stories.jsx
@@ -0,0 +1,39 @@
+import { InputText } from './InputText';
+
+export default {
+  component: InputText,
+  title: 'Input Text',
+  tags: ['autodocs']
+}
+
+export const Default = args => <InputText {...args} />;
+export const Correct = args => <InputText {...args} />;
+export const InCorrect = args => <InputText {...args} />;
+export const Disabled = args => <InputText {...args} />;
+
+Default.args = {
+  correct: false,
+  incorrect: false,
+  value: '',
+  disabled: false,
+  grade: 'g35'
+}
+
+Correct.args = {
+  ...Default.args,
+  correct: true,
+  value: 'answer',
+  disabled: true
+}
+
+InCorrect.args = {
+  ...Default.args,
+  incorrect: true,
+  value: 'answer'
+}
+
+Disabled.args = {
+  ...Default.args,
+  value: 'answer',
+  disabled: true
+}
