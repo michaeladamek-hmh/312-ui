ca0ff0e379ee75b719320f37bfaf7564b8a0f64b
Michael Adamek <michael.adamek@hmhco.com>
2023-09-13 14:57:00 -0400

    Added Drag-and-Drop component.

---
+++ src/components/DragDrop.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+DragDrop.propTypes = {
+  correct: PropTypes.bool,
+  incorrect: PropTypes.bool,
+  disabled: PropTypes.bool,
+  drop: PropTypes.bool,
+	grade: PropTypes.oneOf(['g35', 'g68', 'g912']),
+}
+
+export function DragDrop({ correct, incorrect, disabled, drop, grade }) {
+	const isCorrect = correct ? 'correct' : '';
+	const isIncorrect = incorrect ? 'incorrect' : '';
+
+  return (
+    <div className={ grade }>
+      { drop
+        ? 
+        <div className="drop">
+          <button className="drag">The mouse rides a bike</button>
+        </div>
+        : 
+        <button 
+          className={` drag ${isCorrect} ${isIncorrect} `}
+          disabled={disabled}
+        >
+          The mouse rides a bike
+        </button>
+      }
+    </div>
+  )
+}
---
+++ src/components/DragDrop.stories.jsx
@@ -0,0 +1,45 @@
+import { DragDrop } from './DragDrop';
+
+export default {
+  component: DragDrop,
+  title: 'Drag Drop',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  }
+};
+
+export const Default = args => <DragDrop {...args} />;
+export const Correct = args => <DragDrop {...args} />;
+export const Incorrect = args => <DragDrop {...args} />;
+export const Disabled = args => <DragDrop {...args} />;
+export const Drop = args => <DragDrop {...args} />;
+
+Default.args = {
+  correct: false,
+  incorrect: false,
+  disabled: false,
+  drop: false,
+  grade: 'g35'
+}
+
+Correct.args = {
+  ...Default.args,
+  correct: true,
+  disabled: true
+}
+
+Incorrect.args = {
+  ...Default.args,
+  incorrect: true
+}
+
+Disabled.args = {
+  ...Default.args,
+  disabled: true
+}
+
+Drop.args = {
+  ...Default.args,
+  drop: true
+}
