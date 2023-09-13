874c3146ed983d30b54898d80e15a5f17285f606
Michael Adamek <michael.adamek@hmhco.com>
2023-09-13 14:57:28 -0400

    Added Reveal component.

---
+++ src/components/Reveal.jsx
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+
+Reveal.propTypes = {
+  front: 'Select to Reveal',
+  back: 'Iron Man',
+	grade: PropTypes.oneOf(['g35', 'g68', 'g912']),
+}
+
+export function Reveal({ front, back, grade }) {
+  return (
+    <div className={ grade }>
+      <ul className="source__reveal">
+        <li>
+          <details>
+            <summary>
+              <h5 className="source__flipcards__front">
+                { front }
+              </h5>
+            </summary>
+            <div className="source__flipcards__back">
+              <p>{ back }</p>
+            </div>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>
+              <h5 className="source__flipcards__front">
+                { front }
+              </h5>
+            </summary>
+            <div className="source__flipcards__back">
+              <p>{ back }</p>
+            </div>
+          </details>
+        </li>
+        <li>
+          <details>
+            <summary>
+              <h5 className="source__flipcards__front">
+                { front }
+              </h5>
+            </summary>
+            <div className="source__flipcards__back">
+              <p>{ back }</p>
+            </div>
+          </details>
+        </li>
+      </ul>
+    </div>
+
+  )
+}
---
+++ src/components/Reveal.stories.jsx
@@ -0,0 +1,18 @@
+import { Reveal } from './Reveal';
+
+export default {
+  component: Reveal,
+  title: 'Reveal',
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  }
+};
+
+export const Default = args => <Reveal {...args} />;
+
+Default.args = {
+  front: 'Select to Reveal',
+  back: 'Iron Man',
+	grade: 'g35'
+}
